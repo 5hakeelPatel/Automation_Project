61b8d5ba9b01ec523088fe5071db446456e8ccc8
Shakeelpatel SH <shakeelpatel.1si12is053@gmail.com>
2021-08-30 22:24:54 +0530

    Fixed the if part

--- automation-Script.sh
+++ automation-Script.sh
@@ -8,29 +8,31 @@ timestamp=$(date '+%d%m%Y-%H%M%S')
 
 temp=$( dpkg --get-selections | grep -w -c 'apache2' )
 
-if [[ $tmpe == 0 ]]
+if [[ $tmpe == 0 || $tmpe == "" ]]
 	then
 		echo "appache is not installed"
 		echo "Installing Appache"
 		sudo apt update
 		sudo apt install -qq -y apache2
+fi
 
-else 
-	echo "appache is  available"	
-	status_apache=$( service apache2 status | grep -w -c "Active: active" )
-	
-	echo $status_apache
-	if [[ $status_apache == 1 ]]
-	then echo " Server is Up and running "
-	else
-		echo "Starting Apache2 server"
-		service apache2 start 
-	fi
+ 
+#	echo "appache is  available"	
+
+status_apache=$( service apache2 status | grep -w -c "Active: active" )
+# echo $status_apache
+if [[ $status_apache == 1 ]]
+   then echo " Server is Up and running "
+
+else
+    echo "Starting Apache2 server"
+    service apache2 start 
+fi
 	
 	#Log 
-	tar -cvf /tmp/$user-httpd-logs-$timestamp.tar  /var/log/apache2/*.log 
+tar -cvf /tmp/$user-httpd-logs-$timestamp.tar  /var/log/apache2/*.log 
              #backing up log all files
-        aws s3 cp /tmp/ $s3_bucket  --recursive --exclude "*" --include "*.tar"
-fi
+aws s3 cp /tmp/ $s3_bucket  --recursive --exclude "*" --include "*.tar"
+
 
 
